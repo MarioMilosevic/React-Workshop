d2c51de45cf11ade9e63f3993ede79f4cfe620a5
Mario Milosevic <128989997+MarioMilosevic@users.noreply.github.com>
2024-02-29 14:23:50 +0100

    Separate into components

--- 01-starting-project/01-starting-project/src/App.jsx
+++ 01-starting-project/01-starting-project/src/App.jsx
@@ -1,39 +1,6 @@
-import {CORE_CONCEPTS as data} from "./data";
-import reactImg from "./assets/react-core-concepts.png"
-const reactDescriptions = ["Fundamental", "Crucial", "Core"];
-function getRandomInt(max) {
-  return Math.floor(Math.random() * (max + 1));
-}
-function Header() {
-  const description =
-    reactDescriptions[getRandomInt(reactDescriptions.length - 1)];
-  return (
-    <div>
-      <header>
-        <img src={reactImg} alt="Stylized atom" />
-        <h1>React Essentials</h1>
-        <p>
-          {description} React concepts you will need for almost any app you are
-          going to build!
-        </p>
-      </header>
-      <main>
-        <h2>Time to get started!</h2>
-      </main>
-    </div>
-  );
-}
-
-const CoreConcept = ({ title, description, image }) => {
-  return (
-    <li>
-      <img src={image} alt={title} />
-      <h3>{title}</h3>
-      <p>{description}</p>
-    </li>
-  );
-};
-
+import { CORE_CONCEPTS as data } from "./data";
+import Header from "./components/Header";
+import CoreConcept from "./components/CoreConcept";
 function App() {
   return (
     <div>
@@ -42,14 +9,7 @@ function App() {
         <section id="core-concepts">
           <ul>
             {data.map((el, index) => {
-              return (
-                <CoreConcept
-                  key={index}
-                  title={el.title}
-                  description={el.description}
-                  image={el.image}
-                />
-              );
+              return <CoreConcept key={index} {...el} />;
             })}
           </ul>
         </section>
---
+++ 01-starting-project/01-starting-project/src/components/Header.jsx
@@ -0,0 +1,24 @@
+import reactImg from "../assets/react-core-concepts.png"
+export default function Header() {
+  const reactDescriptions = ["Fundamental", "Crucial", "Core"];
+  function getRandomInt(max) {
+    return Math.floor(Math.random() * (max + 1));
+  }
+  const description =
+    reactDescriptions[getRandomInt(reactDescriptions.length - 1)];
+  return (
+    <div>
+      <header>
+        <img src={reactImg} alt="Stylized atom" />
+        <h1>React Essentials</h1>
+        <p>
+          {description} React concepts you will need for almost any app you are
+          going to build!
+        </p>
+      </header>
+      <main>
+        <h2>Time to get started!</h2>
+      </main>
+    </div>
+  );
+}
