03b33671f30138c09627d501a0f67c7fca6df4ee
Mario Milosevic <128989997+MarioMilosevic@users.noreply.github.com>
2024-03-01 14:52:38 +0100

    Finish exercise

--- src/Button.jsx
+++ src/Button.jsx
@@ -1,7 +1,13 @@
-const Button = (children, {clickHandler}) => {
+const Button = ({ children, clickHandler, position }) => {
+  console.log(position);
   return (
-    <button onClick={clickHandler} className='px-4 py-2'>{children}</button>
-  )
-}
+    <button
+      onClick={clickHandler}
+      className={`px-4 py-2 bg-blue-300 rounded-full mx-auto ${position}`}
+    >
+      {children}
+    </button>
+  );
+};
 
-export default Button
+export default Button;
