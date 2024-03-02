97d194a939a04427f68aecf0b20ff4d6aaf59efd
Mario Milosevic <128989997+MarioMilosevic@users.noreply.github.com>
2024-03-02 16:29:40 +0100

    Refactor code

--- Vjezba dugmad/src/Button.jsx
+++ Vjezba dugmad/src/Button.jsx
@@ -1,15 +1,15 @@
 const Button = ({ clickHandler, type, color }) => {
   let button;
   if (type === "primary") {
-    button = `w-24 h-12 bg-${color}-400 rounded-md text-xl hover:bg-${color}-500`;
+    button = `w-16 h-8 bg-${color}-400 rounded-md text-md hover:bg-${color}-500`;
   } else if (type === "secondary") {
-    button = `w-40 h-20 bg-${color}-400 rounded-md text-2xl hover:bg-${color}-500`;
+    button = `w-24 h-12 bg-${color}-400 rounded-md text-xl hover:bg-${color}-500`;
   } else if (type === "ghost") {
-    button = `w-48 h-24 border border-${color}-400 text-${color}-400 text-3xl duration-200 hover:bg-${color}-400 hover:text-white`;
+    button = `w-32 h-16 border border-${color}-400 text-${color}-400 text-3xl  hover:bg-${color}-400 hover:text-white`;
   }
 
   return (
-    <button className={`${button} rounded-md`} onClick={clickHandler}>
+    <button className={`${button} rounded-md duration-200`} onClick={clickHandler}>
       Save
     </button>
   );
