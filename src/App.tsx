9ae75e603e8088fe784261608561cbf4b65e2f22
Mario Milosevic <128989997+MarioMilosevic@users.noreply.github.com>
2025-05-06 13:14:02 +0200

    Add deleteHandler

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,25 @@
-import React, { useState, useRef } from "react";
-import { dummyData } from "./utils/constants";
+import React, { useState } from "react";
+import { dummyData, dummyType } from "./utils/constants";
 import DummyPerson from "./components/DummyPerson";
 const App = () => {
+  const [persons, setPersons] = useState<dummyType[]>(dummyData);
+
+  const deleteHandler = (id: number) => {
+    setPersons((prev) => {
+      return prev.filter((person) => person.id !== id);
+    });
+  };
+
   return (
     <div className="flex gap-4">
-      {dummyData.map((person) => {
-        return <DummyPerson key={person.id} person={person} />;
+      {persons.map((person) => {
+        return (
+          <DummyPerson
+            key={person.id}
+            person={person}
+            deleteHandler={deleteHandler}
+          />
+        );
       })}
     </div>
   );
--- src/components/DummyPerson.tsx
+++ src/components/DummyPerson.tsx
@@ -2,23 +2,25 @@ import React from "react";
 import { dummyType } from "../utils/constants";
 
 type DummyPersonPropsType = {
-  person: dummyType;
+    person: dummyType;
+    deleteHandler: (id: number) => void; 
 };
 
-const DummyPerson = ({ person }: DummyPersonPropsType) => {
-  console.log(person);
-  const { name, lastName, age } = person;
+const DummyPerson = ({ person, deleteHandler }: DummyPersonPropsType) => {
+  const { name, lastName, age, id } = person;
   return (
-    <div className="border p-2 flex flex-col gap-2">
-      <h1>Name {name}</h1>
-      <h1>Last Name {lastName}</h1>
-          <h1>Age {age}</h1>
-          <div className="flex justify-between items-center">
-          <button>
-              Edit
-          </button>
-          <button>Delete</button>
-          </div>
+    <div className="border p-2 flex flex-col gap-2 min-w-[180px]">
+      <p>Name: {name}</p>
+      <p>Last Name: {lastName}</p>
+      <p>Age: {age}</p>
+      <div className="flex justify-between items-center mt-4">
+        <button className="rounded-xl px-2 py-1 border bg-green-600">
+          Edit
+        </button>
+        <button className="rounded-xl px-2 py-1 border bg-red-700" onClick={() => deleteHandler(id)}>
+          Delete
+        </button>
+      </div>
     </div>
   );
 };
