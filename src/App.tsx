a03b7f9f5357a65c8ada470dd7cd8023aee49dba
Mario Milošević <mario.milosevic@rbt.rs>
2025-08-26 15:41:12 +0200

    Remove extra code

--- src/App.tsx
+++ src/App.tsx
@@ -1,51 +1,6 @@
-import React, { useState } from "react";
-// import { dummyData, dummyType } from "./utils/constants";
-// import PreviewPerson from "./components/PreviewPerson";
-import MarioParent from "./components/MarioParent";
-import MarioChild from "./components/MarioChild";
-
+import { useState } from "react";
 const App = () => {
-  // const [persons, setPersons] = useState<dummyType[]>(dummyData);
-
-  // const deleteHandler = (id: number) => {
-  //   setPersons((prev) => {
-  //     return prev.filter((person) => person.id !== id);
-  //   });
-  // };
-
-  // const editHandler = (editedPerson: dummyType) => {
-  //   console.log(editedPerson);
-  //   setPersons((prev) => {
-  //     return prev.map((person) =>
-  //       person.id === editedPerson.id ? { ...editedPerson } : person
-  //     );
-  //   });
-  // };
-
-  return (
-    <div className="flex gap-4">
-      <MarioParent>
-        <MarioChild />
-        <div>mario</div>
-      </MarioParent>
-      <MarioParent>
-        <h1>naslov</h1>
-        <p>text</p>
-      </MarioParent>
-      <MarioParent>
-        <p>text</p>
-        <h1>naslov</h1>
-      </MarioParent>
-      {/* {persons.map((person) => (
-        <PreviewPerson
-          key={person.id}
-          person={person}
-          deleteHandler={deleteHandler}
-          editHandler={editHandler}
-        />
-      ))} */}
-    </div>
-  );
+  return <div className="flex gap-4">mario</div>;
 };
 
 export default App;
--- src/vjezba/Modal.jsx
+++
@@ -1,5 +0,0 @@
-const Modal = ({color}) => {
-  return <div className={`w-[200px] h-[200px] ${color} mx-auto mb-8`}></div>;
-};
-
-export default Modal;
