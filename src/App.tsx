931b232eff3cbcdb352574329d6fe7effe5ce6f8
Mario Milošević <mario.milosevic@rbt.rs>
2025-08-30 20:33:03 +0200

    Prvi komit sa PSP-1 grane

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,25 @@
-import {
-  QueryClient,
-  QueryClientProvider,
-  useQuery,
-} from "@tanstack/react-query";
-import Example from "./components/Example";
+// import {
+//   QueryClient,
+//   QueryClientProvider,
+//   useQuery,
+// } from "@tanstack/react-query";
+// import Example from "./components/Example";
 
-const queryClient = new QueryClient();
+// const queryClient = new QueryClient();
+import { useState } from "react";
 
 const App = () => {
+  const [counter, setCounter] = useState(0);
+
   return (
-    <QueryClientProvider client={queryClient}>
-      <Example />
-    </QueryClientProvider>
+    <div>
+      <button>Increment +</button>
+      <p>Counter {counter}</p>
+      <button>Decrement -</button>
+    </div>
+    // <QueryClientProvider client={queryClient}>
+    //   <Example />
+    // </QueryClientProvider>
   );
 };
 
